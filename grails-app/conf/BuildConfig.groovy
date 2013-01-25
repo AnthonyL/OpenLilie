237607e8ec78f2d74686f5da1be4dfd67ad8ac67
kazamafury <thierry.weissbeck@gmail.com>
2013-01-25 10:50:18 +0100

    #3 ajout du release plugin pour le deploiement d'artifacts sur des repos maven

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,6 +48,7 @@ grails.project.dependency.resolution = {
         //runtime ":yui-minify-resources:0.1.4"
 
         build ":tomcat:$grailsVersion"
+		build ":release:2.2.0"
 
         runtime ":database-migration:1.1"
 
